4b4a57270c9a16a8a56363a3683d7efb62ca19d7
Jaysig <jasontsigmon@gmail.com>
2017-02-15 16:38:26 -0600

    set up app; so, it imports instead of working on the index file

--- index.ios.js
+++ index.ios.js
@@ -9,63 +9,23 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View,
-  TextInput,
-  TouchableHighlight,
+  View
 } from 'react-native';
 
 export default class Todo extends Component {
-  constructor() {
-    super();
-    this.state = {
-      todos: [1,2,3],
-      newTodo: ''
-    }
-  }
-
-  handleChange(e) {
-    const { value } = e.target;
-    this.setState({newTodo: value})
-
-  }
-
-  handlePress(e) {
-    e.preventDefault();
-    let todos = [...this.state.todos, this.state.newTodo];
-    this.setState({todos, newTodo:''});
-  }
-
-  removeTodo(i){
-    const todos = [...this.state.todos.slice(0,i), this.state.todos.slice(i + 1)]
-    this.setState({todos});
-  }
   render() {
     return (
       <View style={styles.container}>
-        <TextInput
-          onChange={this.handleChange.bind(this)}
-          style={[styles.default, {height: 50}]}
-          value={this.state.newTodo}
-          placeholder="new Todo"
-        />
-        <TouchableHighlight onPress={this.handlePress.bind(this)}>
-          <Text>Create Todo</Text>
-        </TouchableHighlight>
-        {this.state.todos.map((todo, i) =>
-          <Text onPress={() => this.removeTodo.call(this,i)} key={todo}>{todo}</Text>
-        )}
-        {/* <TextInput
-        {...this.props}
-        multiline={true}
-        onChangeText={(text) => {
-          this.setState({text});
-        }}
-        // onContentSizeChange={(event) => {
-        //   this.setState({height: event.nativeEvent.contentSize.height});
-        // }}
-        style={[styles.default, {height: Math.max(35, this.state.height)}]}
-        value={this.state.newTodo}
-      /> */}
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
       </View>
     );
   }
---
+++ src/app/Todo.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
+
+export default class Todo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [1,2,3],
+      newTodo: ''
+    }
+  }
+
+  handleChange(e) {
+    const { value } = e.target;
+    this.setState({newTodo: value})
+
+  }
+
+  handlePress(e) {
+    e.preventDefault();
+    let todos = [...this.state.todos, this.state.newTodo];
+    this.setState({todos, newTodo:''});
+  }
+
+  removeTodo(i){
+    const todos = [...this.state.todos.slice(0,i), this.state.todos.slice(i + 1)]
+    this.setState({todos});
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          onChange={this.handleChange.bind(this)}
+          style={[styles.default, {height: 50}]}
+          value={this.state.newTodo}
+          placeholder="new Todo"
+        />
+        <TouchableHighlight onPress={this.handlePress.bind(this)}>
+          <Text>Create Todo</Text>
+        </TouchableHighlight>
+        {this.state.todos.map((todo, i) =>
+          <Text onPress={() => this.removeTodo.call(this,i)} key={todo}>{todo}</Text>
+        )}
+        {/* <TextInput
+        {...this.props}
+        multiline={true}
+        onChangeText={(text) => {
+          this.setState({text});
+        }}
+        // onContentSizeChange={(event) => {
+        //   this.setState({height: event.nativeEvent.contentSize.height});
+        // }}
+        style={[styles.default, {height: Math.max(35, this.state.height)}]}
+        value={this.state.newTodo}
+      /> */}
+      </View>
+    );
+  }
+}
