9fc118681e006fc65e38c05b2e0120d2012de211
Jaysig <jasontsigmon@gmail.com>
2017-02-15 18:15:47 -0600

    Pulling authors from Reddit

---
+++ src/app/Reddit.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {View, Text} from 'react-native';
+
+export class Reddit extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: []
+    }
+  }
+  componentDidMount() {
+    fetch('https://www.reddit.com/.json', {
+      Accept: 'application/json'
+    })
+    .then(res => res.json())
+    .then(data => this.setState({posts: data.data.children}));
+  }
+  render() {
+    return(
+      <View>
+        <Text>Hello</Text>
+        <View>
+          {this.state.posts.map(post => (
+            <Text>{post.data.author}</Text>
+          ))}
+        </View>
+      </View>
+    )
+  }
+}
